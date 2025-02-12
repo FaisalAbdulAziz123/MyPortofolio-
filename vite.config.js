2ba90de7329ce17830cb93cd9c6e3e3a8a109ced
faisal Az <faisalabdulaziz761@gmail.com>
2025-02-12 14:02:16 +0700

    update perubahan

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public', // Pastikan Vite bisa membaca public/
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'), // Arahkan ke index.html
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
