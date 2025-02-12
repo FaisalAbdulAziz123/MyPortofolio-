112f42bbfca02aa6392a3fe2217deaa528bfe828
faisal Az <faisalabdulaziz761@gmail.com>
2025-02-12 13:52:38 +0700

    update perubahan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import profilePicture from '../assets/faisal.jpg'
-import { FaInstagram } from "react-icons/fa";
-import { FaTiktok } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram, FaTiktok, FaLinkedin, FaFacebookF, FaWhatsapp, FaGithub } from "react-icons/fa";
 import '../styles/Header.css'
+
 function Header() {
     return (
         <header>
             <div className="header-jumbotron">
                 <img src={profilePicture} />
                 <h3>FAISAL ABDUL AZIZ</h3>
-                <p>Programmer - Content Creator - Researcher</p>
+                <p>Programmer - Content Creator - Digital Marketing</p>
                 <div className='socialMedia'>
                     <a href='https://www.instagram.com/isall_ya/'><FaInstagram /></a>
-                    <a href=''><FaTiktok /></a>
-                    <a href=''><FaTwitter /></a>
+                    <a href='https://www.tiktok.com/@abdul_isall?is_from_webapp=1&sender_device=pc'><FaTiktok /></a>
+                    <a href='https://www.linkedin.com/in/faisal-abdul-aziz-72b523338/'><FaLinkedin /></a>
                     <a href='https://web.facebook.com/faisal.abdulaziz.77736?locale=id_ID'><FaFacebookF /></a>
+                    <a href='https://wa.me/6283873645789'><FaWhatsapp /></a>
+                    <a href='https://github.com/FaisalAbdulAziz123?tab=repositories'><FaGithub /></a>
                 </div>
             </div>
         </header>
